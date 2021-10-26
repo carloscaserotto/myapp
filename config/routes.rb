1a22666f951f9777e3bc25602a5b6ec9aeb14c5b
carlos <carlos.caserotto@yahoo.com>
2021-10-26 14:49:42 -0300

    Add delete

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   get 'home', to: 'pages#home'
   get 'about', to: 'pages#about'
 
-  resources :articles, only: [:show, :index, :new, :create, :edit, :update]
+  resources :articles
 end
