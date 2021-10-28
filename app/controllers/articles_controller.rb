a82850af7de8c609da511e88674619755f6a57b0
carlos <carlos.caserotto@yahoo.com>
2021-10-28 15:12:05 -0300

    Controller methods as helper methods

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,7 +21,7 @@ class ArticlesController < ApplicationController
         #render plain: params[:article]
         #render plain: params[:article][:title]
         @article = Article.new(article_params)
-        @article.user = User.first
+        @article.user = current_user
         #render plain:@article.inspect
         if @article.save
             flash[:notice] = "Article successfully created"
