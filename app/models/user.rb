66d66cb57a809eae5285fcc75c4e6562fc07be9d
carlos <carlos.caserotto@yahoo.com>
2021-10-28 10:48:10 -0300

    Add User SignUp

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,5 @@ class User < ApplicationRecord
                         uniqueness: { case_sensitive:false }, 
                         length: { minimum: 3, maximun: 50 },
                         format: { with: VALID_EMAIL_REGEX}
+    has_secure_password
 end
